be26938b31c2cf1206456beb1d56c9360f4bfaad
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-09-04 02:44:32 -0700

    inhib all working and readme updated -- definitely requires latest emer/leabra updates

--- ch3/inhib/inhib.go
+++ ch3/inhib/inhib.go
@@ -17,6 +17,7 @@ import (
 	"github.com/emer/emergent/emer"
 	"github.com/emer/emergent/netview"
 	"github.com/emer/emergent/params"
+	"github.com/emer/emergent/patgen"
 	"github.com/emer/emergent/prjn"
 	"github.com/emer/emergent/relpos"
 	"github.com/emer/etable/eplot"
@@ -49,24 +50,49 @@ var ParamSets = params.Sets{
 			{Sel: "Prjn", Desc: "no learning",
 				Params: params.Params{
 					"Prjn.Learn.Learn": "false",
+					"Prjn.WtInit.Dist": "Uniform",
+					"Prjn.WtInit.Mean": "0.25",
+					"Prjn.WtInit.Var":  "0.2",
 				}},
 			{Sel: "Layer", Desc: "generic params for all layers: lower gain, slower, soft clamp",
 				Params: params.Params{
-					"Layer.Inhib.ActAvg.Init":  "0.35",
+					"Layer.Inhib.Layer.On":     "false",
+					"Layer.Inhib.ActAvg.Init":  "0.2",
 					"Layer.Inhib.ActAvg.Fixed": "true",
-					"Layer.Inhib.Layer.Gi":     "1.4", // need this for FB = 0.5 -- 1 works otherwise but not with adapt
-					"Layer.Inhib.Layer.FB":     "0.5", // this is better for adapt dynamics: 1.0 not as clean of dynamics
-					"Layer.Act.Clamp.Hard":     "false",
-					"Layer.Act.Clamp.Gain":     "0.1",
-					"Layer.Act.Dt.VmTau":       "6", // a bit slower -- not as effective as FBTau
-					"Layer.Act.Noise.Dist":     "Gaussian",
-					"Layer.Act.Noise.Var":      "0.01",
-					"Layer.Act.Noise.Type":     "GeNoise",
-					"Layer.Act.Noise.Fixed":    "false",
-					"Layer.Act.KNa.Slow.Rise":  "0.005",
-					"Layer.Act.KNa.Slow.Max":   "0.2",
-					"Layer.Act.Gbar.K":         "1.2",
-					"Layer.Act.Gbar.L":         "0.1", // this is important relative to .2
+					"Layer.Act.Dt.GTau":        "40",
+					"Layer.Act.Gbar.I":         "0.4",
+					"Layer.Act.Gbar.L":         "0.1",
+				}},
+			{Sel: ".InhibLay", Desc: "generic params for all layers: lower gain, slower, soft clamp",
+				Params: params.Params{
+					"Layer.Act.XX1.Thr": "0.4", // essential for getting active early
+				}},
+			{Sel: ".Inhib", Desc: "inhibitory projections",
+				Params: params.Params{
+					"Prjn.WtInit.Dist": "Uniform",
+					"Prjn.WtInit.Mean": "0.5",
+					"Prjn.WtInit.Var":  "0",
+					"Prjn.WtInit.Sym":  "false",
+				}},
+		},
+	}},
+	{Name: "Untrained", Desc: "simulates untrained weights -- lower variance", Sheets: params.Sheets{
+		"Network": &params.Sheet{
+			{Sel: ".Excite", Desc: "excitatory connections",
+				Params: params.Params{
+					"Prjn.WtInit.Dist": "Uniform",
+					"Prjn.WtInit.Mean": "0.25",
+					"Prjn.WtInit.Var":  "0.2",
+				}},
+		},
+	}},
+	{Name: "Trained", Desc: "simulates trained weights -- higher variance", Sheets: params.Sheets{
+		"Network": &params.Sheet{
+			{Sel: ".Excite", Desc: "excitatory connections",
+				Params: params.Params{
+					"Prjn.WtInit.Dist": "Gaussian",
+					"Prjn.WtInit.Mean": "0.25",
+					"Prjn.WtInit.Var":  "0.7",
 				}},
 		},
 	}},
@@ -78,10 +104,19 @@ var ParamSets = params.Sets{
 // as arguments to methods, and provides the core GUI interface (note the view tags
 // for the fields which provide hints to how things should be displayed).
 type Sim struct {
-	BidirNet   bool              `desc:"if true, use the bidirectionally-connected network -- otherwise use the simpler feedforward network"`
-	Noise      float32           `min:"0" step:"0.01" desc:"the variance parameter for Gaussian noise added to unit activations on every cycle"`
-	KNaAdapt   bool              `desc:"apply sodium-gated potassium adaptation mechanisms that cause the neuron to reduce spiking over time"`
-	CycPerQtr  int               `def:"25,250" desc:"total number of cycles per quarter to run -- increase to 250 when testing adaptation"`
+	BidirNet   bool    `desc:"if true, use the bidirectionally-connected network -- otherwise use the simpler feedforward network"`
+	TrainedWts bool    `desc:"simulate trained weights by having higher variance and Gaussian distributed weight values -- otherwise lower variance, uniform"`
+	InputPct   float32 `def:"20" min:"5" max:"50" step:"1" desc:"percent of active units in input layer (literally number of active units, because input has 100 units total)"`
+	FFFBInhib  bool    `def:"false" desc:"use feedforward, feedback (FFFB) computed inhibition instead of unit-level inhibition"`
+
+	HiddenGbarI       float32 `def:"0.4" min:"0" step:"0.05" desc:"inhibitory conductance strength for inhibition into Hidden layer"`
+	InhibGbarI        float32 `def:"0.75" min:"0" step:"0.05" desc:"inhibitory conductance strength for inhibition into Inhib layer (self-inhibition -- tricky!)"`
+	FFinhibWtScale    float32 `def:"1" min:"0" step:"0.1" desc:"feedforward (FF) inhibition relative strength: for FF projections into Inhib neurons"`
+	FBinhibWtScale    float32 `def:"1" min:"0" step:"0.1" desc:"feedback (FB) inhibition relative strength: for projections into Inhib neurons"`
+	HiddenGTau        float32 `def:"40" min:"1" step:"1" desc:"time constant (tau) for updating G conductances into Hidden neurons -- much slower than std default of 1.4"`
+	InhibGTau         float32 `def:"20" min:"1" step:"1" desc:"time constant (tau) for updating G conductances into Inhib neurons -- much slower than std default of 1.4, but 2x faster than Hidden"`
+	FmInhibWtScaleAbs float32 `def:"1" desc:"absolute weight scaling of projections from inhibition onto hidden and inhib layers -- this must be set to 0 to turn off the connection-based inhibition when using the FFFBInhib computed inbhition"`
+
 	NetFF      *leabra.Network   `view:"no-inline" desc:"the feedforward network -- click to view / edit parameters for layers, prjns, etc"`
 	NetBidir   *leabra.Network   `view:"no-inline" desc:"the bidirectional network -- click to view / edit parameters for layers, prjns, etc"`
 	TstCycLog  *etable.Table     `view:"no-inline" desc:"testing trial-level log data -- click to see record of network's response to each input"`
@@ -89,6 +124,7 @@ type Sim struct {
 	Time       leabra.Time       `desc:"leabra timing parameters and state"`
 	ViewUpdt   leabra.TimeScales `desc:"at what time scale to update the display during testing?  Change to AlphaCyc to make display updating go faster"`
 	TstRecLays []string          `desc:"names of layers to record activations etc of during testing"`
+	Pats       *etable.Table     `view:"no-inline" desc:"the input patterns to use -- randomly generated"`
 
 	// internal state - view:"-"
 	Win          *gi.Window                  `view:"-" desc:"main GUI window"`
@@ -116,14 +152,23 @@ func (ss *Sim) New() {
 	ss.Params = ParamSets
 	ss.ViewUpdt = leabra.Cycle
 	ss.TstRecLays = []string{"Hidden", "Inhib"}
+	ss.Pats = &etable.Table{}
 	ss.Defaults()
 }
 
 // Defaults sets default params
 func (ss *Sim) Defaults() {
-	ss.Noise = 0.01
-	ss.KNaAdapt = false
-	ss.CycPerQtr = 25
+	ss.TrainedWts = false
+	ss.InputPct = 20
+	ss.FFFBInhib = false
+	ss.HiddenGbarI = 0.4
+	ss.InhibGbarI = 0.75
+	ss.FFinhibWtScale = 1
+	ss.FBinhibWtScale = 1
+	ss.HiddenGTau = 40
+	ss.InhibGTau = 20
+	ss.FmInhibWtScaleAbs = 1
+	ss.Time.CycPerQtr = 50
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -131,6 +176,7 @@ func (ss *Sim) Defaults() {
 
 // Config configures all the elements using the standard functions
 func (ss *Sim) Config() {
+	ss.ConfigPats()
 	ss.ConfigNetFF(ss.NetFF)
 	ss.ConfigNetBidir(ss.NetBidir)
 	ss.ConfigTstCycLog(ss.TstCycLog)
@@ -141,11 +187,14 @@ func (ss *Sim) ConfigNetFF(net *leabra.Network) {
 	inp := net.AddLayer2D("Input", 10, 10, emer.Input)
 	hid := net.AddLayer2D("Hidden", 10, 10, emer.Hidden)
 	inh := net.AddLayer2D("Inhib", 10, 2, emer.Hidden)
+	inh.SetClass("InhibLay")
 
-	net.ConnectLayers(inp, hid, prjn.NewFull(), emer.Forward)
+	pj := net.ConnectLayers(inp, hid, prjn.NewFull(), emer.Forward)
+	pj.SetClass("Excite")
 	net.ConnectLayers(inp, inh, prjn.NewFull(), emer.Forward)
 	net.ConnectLayers(hid, inh, prjn.NewFull(), emer.Back)
 	net.ConnectLayers(inh, hid, prjn.NewFull(), emer.Inhib)
+	net.ConnectLayers(inh, inh, prjn.NewFull(), emer.Inhib)
 
 	inh.SetRelPos(relpos.Rel{Rel: relpos.RightOf, Other: "Hidden", YAlign: relpos.Front, Space: 1})
 
@@ -160,23 +209,31 @@ func (ss *Sim) ConfigNetFF(net *leabra.Network) {
 }
 
 func (ss *Sim) ConfigNetBidir(net *leabra.Network) {
-	net.InitName(net, "InhibFF")
+	net.InitName(net, "InhibBidir")
 	inp := net.AddLayer2D("Input", 10, 10, emer.Input)
 	hid := net.AddLayer2D("Hidden", 10, 10, emer.Hidden)
 	inh := net.AddLayer2D("Inhib", 10, 2, emer.Hidden)
+	inh.SetClass("InhibLay")
 	hid2 := net.AddLayer2D("Hidden2", 10, 10, emer.Hidden)
 	inh2 := net.AddLayer2D("Inhib2", 10, 2, emer.Hidden)
+	inh2.SetClass("InhibLay")
 
-	net.ConnectLayers(inp, hid, prjn.NewFull(), emer.Forward)
+	pj := net.ConnectLayers(inp, hid, prjn.NewFull(), emer.Forward)
+	pj.SetClass("Excite")
 	net.ConnectLayers(inp, inh, prjn.NewFull(), emer.Forward)
+	net.ConnectLayers(hid2, inh, prjn.NewFull(), emer.Forward)
 	net.ConnectLayers(hid, inh, prjn.NewFull(), emer.Back)
 	net.ConnectLayers(inh, hid, prjn.NewFull(), emer.Inhib)
+	net.ConnectLayers(inh, inh, prjn.NewFull(), emer.Inhib)
 
-	net.ConnectLayers(hid, hid2, prjn.NewFull(), emer.Forward)
-	net.ConnectLayers(hid2, hid, prjn.NewFull(), emer.Back)
+	pj = net.ConnectLayers(hid, hid2, prjn.NewFull(), emer.Forward)
+	pj.SetClass("Excite")
+	pj = net.ConnectLayers(hid2, hid, prjn.NewFull(), emer.Back)
+	pj.SetClass("Excite")
 	net.ConnectLayers(hid, inh2, prjn.NewFull(), emer.Forward)
 	net.ConnectLayers(hid2, inh2, prjn.NewFull(), emer.Back)
 	net.ConnectLayers(inh2, hid2, prjn.NewFull(), emer.Inhib)
+	net.ConnectLayers(inh2, inh2, prjn.NewFull(), emer.Inhib)
 
 	inh.SetRelPos(relpos.Rel{Rel: relpos.RightOf, Other: "Hidden", YAlign: relpos.Front, Space: 1})
 	hid2.SetRelPos(relpos.Rel{Rel: relpos.Above, Other: "Hidden", YAlign: relpos.Front, XAlign: relpos.Middle})
@@ -197,6 +254,17 @@ func (ss *Sim) InitWts(net *leabra.Network) {
 	net.InitWts()
 }
 
+func (ss *Sim) ConfigPats() {
+	dt := ss.Pats
+	dt.SetMetaData("name", "TrainPats")
+	dt.SetMetaData("desc", "Training patterns")
+	dt.SetFromSchema(etable.Schema{
+		{"Name", etensor.STRING, nil, nil},
+		{"Input", etensor.FLOAT32, []int{10, 10}, []string{"Y", "X"}},
+	}, 1)
+	patgen.PermutedBinaryRows(dt.Cols[1], int(ss.InputPct), 1, 0)
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // 	    Init, utils
 
@@ -204,11 +272,10 @@ func (ss *Sim) InitWts(net *leabra.Network) {
 // and resets the epoch log table
 func (ss *Sim) Init() {
 	ss.Time.Reset()
-	// ss.Time.CycPerQtr = 25 // use full 100 cycles, default
-	ss.InitWts(ss.NetFF)
-	ss.InitWts(ss.NetBidir)
 	ss.StopNow = false
 	ss.SetParams("", false) // all sheets
+	ss.InitWts(ss.NetFF)
+	ss.InitWts(ss.NetBidir)
 	ss.UpdateView()
 }
 
@@ -299,13 +366,9 @@ func (ss *Sim) ApplyInputs() {
 	nt.InitExt() // clear any existing inputs -- not strictly necessary if always
 	// going to the same layers, but good practice and cheap anyway
 
-	// just directly apply all 1s to input
 	ly := nt.LayerByName("Input").(*leabra.Layer)
-	pats := make([]float64, 16)
-	for i := range pats {
-		pats[i] = 1
-	}
-	ly.ApplyExt1D(pats)
+	pat := ss.Pats.CellTensor("Input", 0)
+	ly.ApplyExt(pat)
 }
 
 // Stop tells the sim to stop running
@@ -353,11 +416,56 @@ func (ss *Sim) SetParams(sheet string, setMsg bool) error {
 	}
 	nt := ss.Net()
 	err := ss.SetParamsSet("Base", sheet, setMsg)
-	ly := nt.LayerByName("Input").(*leabra.Layer)
-	ly.Act.Noise.Var = float64(ss.Noise)
-	ly.Act.KNa.On = ss.KNaAdapt
-	ly.Act.Update()
-	ss.Time.CycPerQtr = ss.CycPerQtr
+	if ss.TrainedWts {
+		ss.SetParamsSet("Trained", sheet, setMsg)
+	} else {
+		ss.SetParamsSet("Untrained", sheet, setMsg)
+	}
+	ffinhsc := ss.FFinhibWtScale
+	if nt == ss.NetBidir {
+		ffinhsc *= 0.5 // 2 inhib prjns so .5 ea
+	}
+	hid := nt.LayerByName("Hidden").(*leabra.Layer)
+	hid.Act.Gbar.I = ss.HiddenGbarI
+	hid.Act.Dt.GTau = ss.HiddenGTau
+	hid.Act.Update()
+	inh := nt.LayerByName("Inhib").(*leabra.Layer)
+	inh.Act.Gbar.I = ss.InhibGbarI
+	inh.Act.Dt.GTau = ss.InhibGTau
+	inh.Act.Update()
+	ff := inh.RcvPrjns.SendName("Input").(*leabra.Prjn)
+	ff.WtScale.Rel = ffinhsc
+	fb := inh.RcvPrjns.SendName("Hidden").(*leabra.Prjn)
+	fb.WtScale.Rel = ss.FBinhibWtScale
+	hid.Inhib.Layer.On = ss.FFFBInhib
+	inh.Inhib.Layer.On = ss.FFFBInhib
+	fi := hid.RcvPrjns.SendName("Inhib").(*leabra.Prjn)
+	fi.WtScale.Abs = ss.FmInhibWtScaleAbs
+	fi = inh.RcvPrjns.SendName("Inhib").(*leabra.Prjn)
+	fi.WtScale.Abs = ss.FmInhibWtScaleAbs
+	if nt == ss.NetBidir {
+		hid = nt.LayerByName("Hidden2").(*leabra.Layer)
+		hid.Act.Gbar.I = ss.HiddenGbarI
+		hid.Act.Dt.GTau = ss.HiddenGTau
+		hid.Act.Update()
+		inh = nt.LayerByName("Inhib2").(*leabra.Layer)
+		inh.Act.Gbar.I = ss.InhibGbarI
+		inh.Act.Dt.GTau = ss.InhibGTau
+		inh.Act.Update()
+		hid.Inhib.Layer.On = ss.FFFBInhib
+		inh.Inhib.Layer.On = ss.FFFBInhib
+		fi = hid.RcvPrjns.SendName("Inhib2").(*leabra.Prjn)
+		fi.WtScale.Abs = ss.FmInhibWtScaleAbs
+		fi = inh.RcvPrjns.SendName("Inhib2").(*leabra.Prjn)
+		fi.WtScale.Abs = ss.FmInhibWtScaleAbs
+		ff = inh.RcvPrjns.SendName("Hidden").(*leabra.Prjn)
+		ff.WtScale.Rel = ffinhsc
+		fb = inh.RcvPrjns.SendName("Hidden2").(*leabra.Prjn)
+		fb.WtScale.Rel = ss.FBinhibWtScale
+		inh = nt.LayerByName("Inhib").(*leabra.Layer)
+		ff = inh.RcvPrjns.SendName("Hidden2").(*leabra.Prjn)
+		ff.WtScale.Rel = ffinhsc
+	}
 	return err
 }
 
@@ -404,18 +512,9 @@ func (ss *Sim) LogTstCyc(dt *etable.Table, cyc int) {
 	// ly := nt.LayerByName("Input").(*leabra.Layer)
 	dt.SetCellFloat("Cycle", row, float64(cyc))
 
-	if ss.LayRecTsr == nil {
-		ss.LayRecTsr = make(map[string]*etensor.Float32)
-	}
 	for _, lnm := range ss.TstRecLays {
-		tsr, ok := ss.LayRecTsr[lnm]
-		if !ok {
-			tsr = &etensor.Float32{}
-			ss.LayRecTsr[lnm] = tsr
-		}
 		ly := nt.LayerByName(lnm).(*leabra.Layer)
-		ly.UnitValsTensor(tsr, "Act")
-		dt.SetCellTensor(lnm, row, tsr)
+		dt.SetCellFloat(lnm+"ActAvg", row, float64(ly.Pools[0].Inhib.Act.Avg))
 	}
 
 	// note: essential to use Go version of update when called from another goroutine
@@ -425,21 +524,17 @@ func (ss *Sim) LogTstCyc(dt *etable.Table, cyc int) {
 }
 
 func (ss *Sim) ConfigTstCycLog(dt *etable.Table) {
-	nt := ss.Net()
 	dt.SetMetaData("name", "TstCycLog")
 	dt.SetMetaData("desc", "Record of testing per cycle")
 	dt.SetMetaData("read-only", "true")
 	dt.SetMetaData("precision", strconv.Itoa(LogPrec))
 
-	ncy := 100 // max cycles
+	ncy := 200 // max cycles
 	sch := etable.Schema{
 		{"Cycle", etensor.INT64, nil, nil},
-		{"TrialName", etensor.STRING, nil, nil},
-		{"Harmony", etensor.FLOAT64, nil, nil},
 	}
 	for _, lnm := range ss.TstRecLays {
-		ly := nt.LayerByName(lnm).(*leabra.Layer)
-		sch = append(sch, etable.Column{lnm, etensor.FLOAT64, ly.Shp.Shp, nil})
+		sch = append(sch, etable.Column{lnm + "ActAvg", etensor.FLOAT64, nil, nil})
 	}
 	dt.SetFromSchema(sch, ncy)
 }
@@ -450,11 +545,9 @@ func (ss *Sim) ConfigTstCycPlot(plt *eplot.Plot2D, dt *etable.Table) *eplot.Plot
 	plt.SetTable(dt)
 	// order of params: on, fixMin, min, fixMax, max
 	plt.SetColParams("Cycle", false, true, 0, false, 0)
-	plt.SetColParams("TrialName", false, true, 0, false, 0)
-	plt.SetColParams("Harmony", true, true, 0, true, .25)
 
 	for _, lnm := range ss.TstRecLays {
-		plt.SetColParams(lnm, false, true, 0, true, 1)
+		plt.SetColParams(lnm+"ActAvg", true, true, 0, true, 1)
 	}
 	return plt
 }
@@ -497,12 +590,14 @@ feedforward and feedback inhibition to excitatory pyramidal neurons.
 
 	nv := tv.AddNewTab(netview.KiT_NetView, "FF Net").(*netview.NetView)
 	nv.Var = "Act"
+	nv.Params.MaxRecs = 200
 	nv.SetNet(ss.NetFF)
 	ss.NetViewFF = nv
 	nv.ViewDefaults()
 
 	nv = tv.AddNewTab(netview.KiT_NetView, "Bidir Net").(*netview.NetView)
 	nv.Var = "Act"
+	nv.Params.MaxRecs = 200
 	nv.SetNet(ss.NetBidir)
 	ss.NetViewBidir = nv
 
@@ -529,12 +624,17 @@ feedforward and feedback inhibition to excitatory pyramidal neurons.
 	}}, win.This(), func(recv, send ki.Ki, sig int64, data interface{}) {
 		if !ss.IsRunning {
 			ss.IsRunning = true
-			if ss.CycPerQtr == 25 {
-				ss.TestTrial() // show every update
-			} else {
-				go ss.TestTrial() // fast..
-			}
+			ss.TestTrial() // show every update
 			ss.IsRunning = false
+			vp.SetNeedsFullRender()
+		}
+	})
+
+	tbar.AddAction(gi.ActOpts{Label: "Config Pats", Icon: "update", Tooltip: "Generates a new input pattern based on current InputPct amount.", UpdateFunc: func(act *gi.Action) {
+		act.SetActiveStateUpdt(!ss.IsRunning)
+	}}, win.This(), func(recv, send ki.Ki, sig int64, data interface{}) {
+		if !ss.IsRunning {
+			ss.ConfigPats()
 			vp.SetNeedsFullRender()
 		}
 	})
