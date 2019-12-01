b792b3a9d286c458ebad7f6bbaf0aeb89cd9621b
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-11-30 16:26:12 -0800

    add top-level docs.go so go get is happy

---
+++ doc.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2019, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/* Package sims are the neural network simulation models for the [CCN Textbook](https://grey.colorado.edu/CompCogNeuro/index.php/CCNBook/Main).
+
+These models are implemented in the new *Go* (golang) version of
+[emergent](https://github.com/emer/emergent), with Python versions
+available as well (note: not yet!).
+
+This github repository contains the full source code and you can build
+and run the models by cloning the repository and building / running
+he individual projects as described in the emergent Wiki help page: [Wiki Install](https://github.com/emer/emergent/wiki/Install).
+*/
+package sims
