e50d7ef806456e7f9c48b947d6028f2c859456d9
thazy <thazy@colorado.edu>
2020-11-18 17:20:19 -0700

    initial add of pvlv directory to chapter 7 - requires emer/emergent v1.1.13-0.20201117122856-0c7b7c2f304a and go mod vendor

---
+++ ch7/pvlv/data/trial_instance_recs.go
@@ -0,0 +1,60 @@
+package data
+
+import "github.com/emer/leabra/pvlv"
+
+// A set of trial groups, sourced from an TrialParams list, instantiated according to the
+// PercentOfTotal field in the source list.
+// this is what we get after calling SetActiveTrialList
+// Still not fully instantiated, US is still a probability
+type TrialInstance struct {
+	TrialName            string
+	ValenceContext       pvlv.Valence
+	USFlag               bool
+	TestFlag             bool
+	MixedUS              bool
+	USProb               float64
+	USMagnitude          float64
+	AlphaTicksPerTrialGp int
+	CS                   string
+	CSTimeStart          int
+	CSTimeEnd            int
+	CS2TimeStart         int
+	CS2TimeEnd           int
+	USTimeStart          int
+	USTimeEnd            int
+	Context              string
+	USType               string
+}
+
+type TrialInstanceList []*TrialInstance
+
+type TrialInstanceRecs struct {
+	Recs
+}
+
+func NewTrialInstanceRecs(til *TrialInstanceList) *TrialInstanceRecs {
+	if til == nil {
+		til = new(TrialInstanceList)
+	}
+	recs := &TrialInstanceRecs{Recs: *NewRecs(til)}
+	return recs
+}
+
+func (til *TrialInstanceList) Length() int {
+	return len(*til)
+}
+
+func (til *TrialInstanceList) Append(ins interface{}) IRecs {
+	ret := append(*til, ins.(*TrialInstance))
+	return &ret
+}
+
+func (til *TrialInstanceList) Get(i int) interface{} {
+	return (*til)[i]
+}
+
+func (til *TrialInstanceRecs) ReadNext() *TrialInstance {
+	return til.Recs.ReadNext().(*TrialInstance)
+}
+
+var _ IRecs = (*TrialInstanceList)(nil) // check for interface implementation
