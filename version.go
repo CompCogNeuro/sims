a077b43638e0c41137dc489c12ddd4e3b4d827e2
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2021-03-19 02:52:25 -0700

    v1.2.5 release

--- version.go
+++ version.go
@@ -3,8 +3,8 @@
 package sims
 
 const (
-	Version = "v1.2.4"
-	GitCommit = "1b1766e" // the commit JUST BEFORE the release
-	VersionDate = "2021-02-11 14:37" // UTC
+	Version = "v1.2.5"
+	GitCommit = "7b535e1" // the commit JUST BEFORE the release
+	VersionDate = "2021-03-19 09:52" // UTC
 )
 
