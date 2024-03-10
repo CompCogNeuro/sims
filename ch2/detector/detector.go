7a305334fb3412efc7a95d80e7b6805ff93cffe8
Kai O'Reilly <koreilly5297@gmail.com>
2024-03-09 17:24:01 -0800

    replace detector bindata with go embed

--- ch2/detector/detector.go
+++ ch2/detector/detector.go
@@ -9,6 +9,7 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"fmt"
 	"log"
 	"strconv"
@@ -17,6 +18,7 @@ import (
 	"cogentcore.org/core/gi"
 	"cogentcore.org/core/gimain"
 	"cogentcore.org/core/giv"
+	"cogentcore.org/core/grr"
 	"cogentcore.org/core/ki"
 	"cogentcore.org/core/kit"
 	"cogentcore.org/core/mat32"
@@ -40,6 +42,9 @@ func main() {
 
 // LogPrec is precision for saving float values in logs
 const LogPrec = 4
+
+//go:embed digits.tsv
+var digits []byte
 
 // ParamSets is the default set of parameters -- Base is always applied, and others can be optionally
 // selected to apply on top of that
@@ -391,15 +396,8 @@ func (ss *Sim) SetParamsSet(setNm string, sheet string, setMsg bool) error {
 func (ss *Sim) OpenPatAsset(dt *etable.Table, fnm, name, desc string) error {
 	dt.SetMetaData("name", name)
 	dt.SetMetaData("desc", desc)
-	ab, err := Asset(fnm)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	err = dt.ReadCSV(bytes.NewBuffer(ab), etable.Tab)
-	if err != nil {
-		log.Println(err)
-	} else {
+	err := dt.ReadCSV(bytes.NewBuffer(digits), etable.Tab)
+	if grr.Log(err) == nil {
 		for i := 1; i < len(dt.Cols); i++ {
 			dt.Cols[i].SetMetaData("grid-fill", "0.9")
 		}
