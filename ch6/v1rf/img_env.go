d163127697c845465b80076c7e17693ccdd1185b
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-09-29 01:38:49 -0700

    v1rf first pass working

---
+++ ch6/v1rf/img_env.go
@@ -0,0 +1,174 @@
+// Copyright (c) 2019, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"image"
+	"io/fs"
+	"log"
+	"math/rand"
+
+	"cogentcore.org/core/base/iox/imagex"
+	"cogentcore.org/core/tensor"
+	"github.com/anthonynsimon/bild/clone"
+	"github.com/emer/emergent/v2/env"
+	"github.com/emer/emergent/v2/etime"
+	"github.com/emer/vision/v2/vxform"
+)
+
+// ImgEnv presents images from a list of image files, using V1 simple and complex filtering.
+// images are just selected at random each trial -- nothing fancy here.
+type ImgEnv struct {
+	// name of this environment
+	Name string
+	// paths to images
+	ImageFiles []string
+	// images (preload for speed)
+	Images []*image.RGBA
+	// current image index
+	ImageIndex env.CurPrvInt
+	// visual processing params
+	Vis Vis
+	// random transform parameters
+	XFormRand vxform.Rand
+	// current -- prev transforms
+	XForm vxform.XForm
+	// current run of model as provided during Init
+	Trial env.Counter `view:"inline"`
+	// original image prior to random transforms
+	OrigImg tensor.Float32
+}
+
+func (ev *ImgEnv) Label() string { return ev.Name }
+
+func (ev *ImgEnv) State(element string) tensor.Tensor {
+	switch element {
+	case "Image":
+		return &ev.Vis.ImgTsr
+	case "LGN":
+		return &ev.Vis.OutTsr
+	case "LGNon":
+		return ev.Vis.OutTsr.SubSpace([]int{0})
+	case "LGNoff":
+		return ev.Vis.OutTsr.SubSpace([]int{1})
+	}
+	return nil
+}
+
+func (ev *ImgEnv) Defaults() {
+	ev.Vis.Defaults()
+	ev.XFormRand.TransX.Set(0, 0) // translation happens in random chunk selection
+	ev.XFormRand.TransY.Set(0, 0)
+	ev.XFormRand.Scale.Set(0.5, 1)
+	ev.XFormRand.Rot.Set(-90, 90)
+}
+
+func (ev *ImgEnv) Init(run int) {
+	ev.Trial.Scale = etime.Trial
+	ev.Trial.Init()
+	ev.Trial.Cur = -1 // init state -- key so that first Step() = 0
+}
+
+func (ev *ImgEnv) Step() bool {
+	ev.Trial.Incr()
+	ev.PickRndImage()
+	ev.FilterImg()
+	// debug only:
+	// img := ev.Images[ev.ImageIndex.Cur]
+	// vfilter.RGBToGrey(img, &ev.OrigImg, 0, false) // pad for filt, bot zero
+	return true
+}
+
+// DoImage processes specified image number
+func (ev *ImgEnv) DoImage(imgNo int) {
+	ev.ImageIndex.Set(imgNo)
+	ev.FilterImg()
+}
+
+func (ev *ImgEnv) Action(element string, input tensor.Tensor) {
+	// nop
+}
+
+// Compile-time check that implements Env interface
+var _ env.Env = (*ImgEnv)(nil)
+
+// String returns the string rep of the LED env state
+func (ev *ImgEnv) String() string {
+	cfn := ev.ImageFiles[ev.ImageIndex.Cur]
+	return fmt.Sprintf("Obj: %s, %s", cfn, ev.XForm.String())
+}
+
+// PickRndImage picks an image at random
+func (ev *ImgEnv) PickRndImage() {
+	nimg := len(ev.Images)
+	ev.ImageIndex.Set(rand.Intn(nimg))
+}
+
+// FilterImg filters the image using new random xforms
+func (ev *ImgEnv) FilterImg() {
+	ev.XFormRand.Gen(&ev.XForm)
+	oimg := ev.Images[ev.ImageIndex.Cur]
+	// following logic first extracts a sub-image of 2x the ultimate filtered size of image
+	// from original image, which greatly speeds up the xform processes, relative to working
+	// on entire 800x600 original image
+	insz := ev.Vis.Geom.In.Mul(2) // target size * 2
+	ibd := oimg.Bounds()
+	isz := ibd.Size()
+	irng := isz.Sub(insz)
+	var st image.Point
+	st.X = rand.Intn(irng.X)
+	st.Y = rand.Intn(irng.Y)
+	ed := st.Add(insz)
+	simg := oimg.SubImage(image.Rectangle{Min: st, Max: ed})
+	img := ev.XForm.Image(simg)
+	ev.Vis.Filter(img)
+}
+
+// OpenImages opens all the images
+func (ev *ImgEnv) OpenImages() error {
+	nimg := len(ev.ImageFiles)
+	if len(ev.Images) != nimg {
+		ev.Images = make([]*image.RGBA, nimg)
+	}
+	var lsterr error
+	for i, fn := range ev.ImageFiles {
+		img, _, err := imagex.Open(fn)
+		if err != nil {
+			log.Println(err)
+			lsterr = err
+			continue
+		}
+		if rg, ok := img.(*image.RGBA); ok {
+			ev.Images[i] = rg
+		} else {
+			ev.Images[i] = clone.AsRGBA(img)
+		}
+	}
+	return lsterr
+}
+
+// OpenImagesFS opens all the images as assets
+func (ev *ImgEnv) OpenImagesFS(fsys fs.FS) error {
+	nimg := len(ev.ImageFiles)
+	if len(ev.Images) != nimg {
+		ev.Images = make([]*image.RGBA, nimg)
+	}
+	var lsterr error
+	for i, fn := range ev.ImageFiles {
+		img, _, err := imagex.OpenFS(fsys, fn)
+		if err != nil {
+			log.Println(err)
+			lsterr = err
+			continue
+		}
+		if rg, ok := img.(*image.RGBA); ok {
+			ev.Images[i] = rg
+		} else {
+			ev.Images[i] = clone.AsRGBA(img)
+		}
+	}
+	return lsterr
+}
---
+++ ch6/v1rf/v1rf.go
@@ -0,0 +1,585 @@
+// Copyright (c) 2024, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+v1rf illustrates how self-organizing learning in response to natural images
+produces the oriented edge detector receptive field properties of neurons
+in primary visual cortex (V1). This provides insight into why the visual
+system encodes information in the way it does, while also providing an
+important test of the biological relevance of our computational models.
+*/
+package main
+
+//go:generate core generate -add-types
+
+import (
+	"embed"
+	"fmt"
+	"os"
+
+	"cogentcore.org/core/base/errors"
+	"cogentcore.org/core/base/randx"
+	"cogentcore.org/core/core"
+	"cogentcore.org/core/icons"
+	"cogentcore.org/core/math32"
+	"cogentcore.org/core/tensor/table"
+	"cogentcore.org/core/tensor/tensorcore"
+	"cogentcore.org/core/tree"
+	"github.com/emer/emergent/v2/econfig"
+	"github.com/emer/emergent/v2/egui"
+	"github.com/emer/emergent/v2/elog"
+	"github.com/emer/emergent/v2/emer"
+	"github.com/emer/emergent/v2/env"
+	"github.com/emer/emergent/v2/estats"
+	"github.com/emer/emergent/v2/etime"
+	"github.com/emer/emergent/v2/looper"
+	"github.com/emer/emergent/v2/netview"
+	"github.com/emer/emergent/v2/params"
+	"github.com/emer/emergent/v2/paths"
+	"github.com/emer/leabra/v2/leabra"
+)
+
+//go:embed v1rf_img1.jpg v1rf_img2.jpg v1rf_img3.jpg v1rf_img4.jpg v1rf_rec2.wts.gz v1rf_rec05.wts.gz probes.tsv
+var content embed.FS
+
+func main() {
+	sim := &Sim{}
+	sim.New()
+	sim.ConfigAll()
+	sim.RunGUI()
+}
+
+// see params.go for params, config.go for Config
+
+// Sim encapsulates the entire simulation model, and we define all the
+// functionality as methods on this struct.  This structure keeps all relevant
+// state information organized and available without having to pass everything around
+// as arguments to methods, and provides the core GUI interface (note the view tags
+// for the fields which provide hints to how things should be displayed).
+type Sim struct {
+	// excitatory lateral (recurrent) WtScale.Rel value
+	ExcitLateralScale float32 `def:"0.2"`
+
+	// inhibitory lateral (recurrent) WtScale.Abs value
+	InhibLateralScale float32 `def:"0.2"`
+
+	// do excitatory lateral (recurrent) connections learn?
+	ExcitLateralLearn bool `def:"true"`
+
+	// simulation configuration parameters -- set by .toml config file and / or args
+	Config Config `new-window:"+"`
+
+	// the network -- click to view / edit parameters for layers, paths, etc
+	Net *leabra.Network `new-window:"+" display:"no-inline"`
+
+	// all parameter management
+	Params emer.NetParams `display:"add-fields"`
+
+	// testing probe input paterns
+	Probes *table.Table `new-window:"+" display:"no-inline"`
+
+	// contains looper control loops for running sim
+	Loops *looper.Manager `new-window:"+" display:"no-inline"`
+
+	// contains computed statistic values
+	Stats estats.Stats `new-window:"+"`
+
+	// Contains all the logs and information about the logs.'
+	Logs elog.Logs `new-window:"+"`
+
+	// Environments
+	Envs env.Envs `new-window:"+" display:"no-inline"`
+
+	// leabra timing parameters and state
+	Context leabra.Context `new-window:"+"`
+
+	// netview update parameters
+	ViewUpdate netview.ViewUpdate `display:"add-fields"`
+
+	// manages all the gui elements
+	GUI egui.GUI `display:"-"`
+
+	// a list of random seeds to use for each run
+	RandSeeds randx.Seeds `display:"-"`
+}
+
+// New creates new blank elements and initializes defaults
+func (ss *Sim) New() {
+	ss.Config.Defaults()
+	ss.Defaults()
+	econfig.Config(&ss.Config, "config.toml")
+	ss.Net = leabra.NewNetwork("V1RF")
+	ss.Probes = table.NewTable()
+	ss.Params.Config(ParamSets, ss.Config.Params.Sheet, ss.Config.Params.Tag, ss.Net)
+	ss.Stats.Init()
+	ss.RandSeeds.Init(100) // max 100 runs
+	ss.InitRandSeed(0)
+	ss.Context.Defaults()
+}
+
+func (ss *Sim) Defaults() {
+	ss.ExcitLateralScale = 0.2
+	ss.InhibLateralScale = 0.2
+	ss.ExcitLateralLearn = true
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// 		Configs
+
+// Config configures all the elements using the standard functions
+func (ss *Sim) ConfigAll() {
+	ss.OpenPatterns()
+	ss.ConfigEnv()
+	ss.ConfigNet(ss.Net)
+	ss.ConfigLogs()
+	ss.ConfigLoops()
+	if ss.Config.Params.SaveAll {
+		ss.Config.Params.SaveAll = false
+		ss.Net.SaveParamsSnapshot(&ss.Params.Params, &ss.Config, ss.Config.Params.Good)
+		os.Exit(0)
+	}
+}
+
+func (ss *Sim) OpenPatterns() {
+	ss.Probes.SetMetaData("name", "Probes")
+	ss.Probes.SetMetaData("desc", "Probes testing patterns")
+	errors.Log(ss.Probes.OpenFS(content, "probes.tsv", table.Tab))
+}
+
+func (ss *Sim) ConfigEnv() {
+	// Can be called multiple times -- don't re-create
+	var trn *ImgEnv
+	var tst *env.FixedTable
+	if len(ss.Envs) == 0 {
+		trn = &ImgEnv{}
+		tst = &env.FixedTable{}
+	} else {
+		trn = ss.Envs.ByMode(etime.Train).(*ImgEnv)
+		tst = ss.Envs.ByMode(etime.Test).(*env.FixedTable)
+	}
+
+	trn.Name = etime.Train.String()
+	trn.Defaults()
+	trn.Trial.Max = ss.Config.Run.NTrials
+	trn.ImageFiles = []string{"v1rf_img1.jpg", "v1rf_img2.jpg", "v1rf_img3.jpg", "v1rf_img4.jpg"}
+	trn.OpenImagesFS(content)
+	if ss.Config.Env.Env != nil {
+		params.ApplyMap(trn, ss.Config.Env.Env, ss.Config.Debug)
+	}
+
+	tst.Name = etime.Test.String()
+	tst.Trial.Max = ss.Config.Run.NTrials
+	tst.Table = table.NewIndexView(ss.Probes)
+
+	trn.Init(0)
+	tst.Init(0)
+
+	ss.Envs.Add(trn, tst)
+}
+
+func (ss *Sim) ConfigNet(net *leabra.Network) {
+	net.SetRandSeed(ss.RandSeeds[0]) // init new separate random seed, using run = 0
+
+	lgnOn := net.AddLayer2D("LGNon", 12, 12, leabra.InputLayer)
+	lgnOff := net.AddLayer2D("LGNoff", 12, 12, leabra.InputLayer)
+	v1 := net.AddLayer2D("V1", 14, 14, leabra.SuperLayer)
+
+	full := paths.NewFull()
+	net.ConnectLayers(lgnOn, v1, full, leabra.ForwardPath)
+	net.ConnectLayers(lgnOff, v1, full, leabra.ForwardPath)
+
+	circ := paths.NewCircle()
+	circ.TopoWeights = true
+	circ.Radius = 4
+	circ.Sigma = .75
+
+	rec := net.ConnectLayers(v1, v1, circ, leabra.LateralPath)
+	rec.AddClass("ExciteLateral")
+
+	inh := net.ConnectLayers(v1, v1, full, leabra.InhibPath)
+	inh.AddClass("InhibLateral")
+
+	lgnOff.PlaceRightOf(lgnOn, 2)
+	v1.PlaceAbove(lgnOn)
+	v1.Pos.XOffset = 5
+
+	net.Build()
+	net.Defaults()
+	ss.ApplyParams()
+	net.InitWeights()
+}
+
+func (ss *Sim) ApplyParams() {
+	ss.Params.SetAll() // first hard-coded defaults
+	nt := ss.Net
+	v1 := nt.LayerByName("V1")
+	elat := v1.RecvPaths[2]
+	elat.WtScale.Rel = ss.ExcitLateralScale
+	elat.Learn.Learn = ss.ExcitLateralLearn
+	ilat := v1.RecvPaths[3]
+	ilat.WtScale.Abs = ss.InhibLateralScale
+
+	if ss.Config.Params.Network != nil {
+		ss.Params.SetNetworkMap(ss.Net, ss.Config.Params.Network)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// 	    Init, utils
+
+// Init restarts the run, and initializes everything, including network weights
+// and resets the epoch log table
+func (ss *Sim) Init() {
+	if ss.Config.GUI {
+		ss.Stats.SetString("RunName", ss.Params.RunName(0)) // in case user interactively changes tag
+	}
+	ss.Loops.ResetCounters()
+	ss.InitRandSeed(0)
+	// ss.ConfigEnv() // re-config env just in case a different set of patterns was
+	// selected or patterns have been modified etc
+	ss.GUI.StopNow = false
+	ss.ApplyParams()
+	ss.NewRun()
+	ss.ViewUpdate.RecordSyns()
+	ss.ViewUpdate.Update()
+}
+
+// InitRandSeed initializes the random seed based on current training run number
+func (ss *Sim) InitRandSeed(run int) {
+	ss.RandSeeds.Set(run)
+	ss.RandSeeds.Set(run, &ss.Net.Rand)
+}
+
+// ConfigLoops configures the control loops: Training, Testing
+func (ss *Sim) ConfigLoops() {
+	man := looper.NewManager()
+
+	trls := ss.Config.Run.NTrials
+
+	man.AddStack(etime.Train).
+		AddTime(etime.Run, ss.Config.Run.NRuns).
+		AddTime(etime.Epoch, ss.Config.Run.NEpochs).
+		AddTime(etime.Trial, trls).
+		AddTime(etime.Cycle, 100)
+
+	man.AddStack(etime.Test).
+		AddTime(etime.Epoch, 1).
+		AddTime(etime.Trial, ss.Probes.Rows).
+		AddTime(etime.Cycle, 100)
+
+	leabra.LooperStdPhases(man, &ss.Context, ss.Net, 75, 99)                // plus phase timing
+	leabra.LooperSimCycleAndLearn(man, ss.Net, &ss.Context, &ss.ViewUpdate) // std algo code
+
+	for mode := range man.Stacks {
+		mode := mode // For closures
+		stack := man.Stacks[mode]
+		stack.Loops[etime.Trial].OnStart.Add("ApplyInputs", func() {
+			ss.ApplyInputs()
+		})
+	}
+
+	man.GetLoop(etime.Train, etime.Run).OnStart.Add("NewRun", ss.NewRun)
+
+	// Add Testing
+	trainEpoch := man.GetLoop(etime.Train, etime.Epoch)
+	trainEpoch.OnStart.Add("TestAtInterval", func() {
+		if (ss.Config.Run.TestInterval > 0) && ((trainEpoch.Counter.Cur+1)%ss.Config.Run.TestInterval == 0) {
+			// Note the +1 so that it doesn't occur at the 0th timestep.
+			ss.TestAll()
+		}
+	})
+
+	/////////////////////////////////////////////
+	// Logging
+
+	man.GetLoop(etime.Test, etime.Epoch).OnEnd.Add("LogTestErrors", func() {
+		leabra.LogTestErrors(&ss.Logs)
+	})
+
+	man.AddOnEndToAll("Log", ss.Log)
+	leabra.LooperResetLogBelow(man, &ss.Logs)
+
+	// Save weights to file, to look at later
+	man.GetLoop(etime.Train, etime.Run).OnEnd.Add("SaveWeights", func() {
+		ctrString := ss.Stats.PrintValues([]string{"Run", "Epoch"}, []string{"%03d", "%05d"}, "_")
+		leabra.SaveWeightsIfConfigSet(ss.Net, ss.Config.Log.SaveWeights, ctrString, ss.Stats.String("RunName"))
+	})
+
+	////////////////////////////////////////////
+	// GUI
+
+	if !ss.Config.GUI {
+		if ss.Config.Log.NetData {
+			man.GetLoop(etime.Test, etime.Trial).Main.Add("NetDataRecord", func() {
+				ss.GUI.NetDataRecord(ss.ViewUpdate.Text)
+			})
+		}
+	} else {
+		man.GetLoop(etime.Test, etime.Trial).OnEnd.Add("ActRFs", func() {
+			ss.Stats.UpdateActRFs(ss.Net, "ActM", 0.01, 0)
+		})
+		man.GetLoop(etime.Train, etime.Trial).OnStart.Add("UpdateImage", func() {
+			ss.GUI.Grid("Image").NeedsRender()
+		})
+		man.GetLoop(etime.Test, etime.Trial).OnStart.Add("UpdateImage", func() {
+			ss.GUI.Grid("Image").NeedsRender()
+		})
+
+		leabra.LooperUpdateNetView(man, &ss.ViewUpdate, ss.Net, ss.NetViewCounters)
+		leabra.LooperUpdatePlots(man, &ss.GUI)
+	}
+
+	if ss.Config.Debug {
+		fmt.Println(man.DocString())
+	}
+	ss.Loops = man
+}
+
+// ApplyInputs applies input patterns from given environment.
+// It is good practice to have this be a separate method with appropriate
+// args so that it can be used for various different contexts
+// (training, testing, etc).
+func (ss *Sim) ApplyInputs() {
+	ctx := &ss.Context
+	net := ss.Net
+	ev := ss.Envs.ByMode(ctx.Mode).(*ImgEnv)
+	net.InitExt()
+	lays := net.LayersByType(leabra.InputLayer, leabra.TargetLayer)
+	ev.Step()
+
+	// ss.Stats.SetString("TrialName", ev.String())
+	for _, lnm := range lays {
+		ly := ss.Net.LayerByName(lnm)
+		pats := ev.State(ly.Name)
+		if pats != nil {
+			ly.ApplyExt(pats)
+		}
+	}
+}
+
+// NewRun intializes a new run of the model, using the TrainEnv.Run counter
+// for the new run value
+func (ss *Sim) NewRun() {
+	ctx := &ss.Context
+	ss.InitRandSeed(ss.Loops.GetLoop(etime.Train, etime.Run).Counter.Cur)
+	ss.Envs.ByMode(etime.Train).Init(0)
+	ss.Envs.ByMode(etime.Test).Init(0)
+	ctx.Reset()
+	ctx.Mode = etime.Train
+	ss.Net.InitWeights()
+	ss.InitStats()
+	ss.StatCounters()
+	ss.Logs.ResetLog(etime.Train, etime.Epoch)
+	ss.Logs.ResetLog(etime.Test, etime.Epoch)
+}
+
+// TestAll runs through the full set of testing items
+func (ss *Sim) TestAll() {
+	ss.Envs.ByMode(etime.Test).Init(0)
+	ss.Stats.ActRFs.Reset()
+	ss.Loops.ResetAndRun(etime.Test)
+	ss.Loops.Mode = etime.Train // Important to reset Mode back to Train because this is called from within the Train Run.
+	ss.Stats.ActRFsAvgNorm()
+	ss.GUI.ViewActRFs(&ss.Stats.ActRFs)
+
+}
+
+// RunTestAll runs through the full set of testing items, has stop running = false at end -- for gui
+func (ss *Sim) RunTestAll() {
+	ss.Logs.ResetLog(etime.Test, etime.Epoch) // only show last row
+	ss.GUI.StopNow = false
+	ss.TestAll()
+	ss.GUI.Stopped()
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// 		Stats
+
+// InitStats initializes all the statistics.
+// called at start of new run
+func (ss *Sim) InitStats() {
+	ss.Stats.SetString("TrialName", "0")
+}
+
+// StatCounters saves current counters to Stats, so they are available for logging etc
+// Also saves a string rep of them for ViewUpdate.Text
+func (ss *Sim) StatCounters() {
+	ctx := &ss.Context
+	mode := ctx.Mode
+	ss.Loops.Stacks[mode].CountersToStats(&ss.Stats)
+	// always use training epoch..
+	trnEpc := ss.Loops.Stacks[etime.Train].Loops[etime.Epoch].Counter.Cur
+	ss.Stats.SetInt("Epoch", trnEpc)
+	trl := ss.Stats.Int("Trial")
+	ss.Stats.SetInt("Trial", trl)
+	ss.Stats.SetInt("Cycle", int(ctx.Cycle))
+	ss.Stats.SetString("TrialName", ss.Stats.String("TrialName"))
+}
+
+func (ss *Sim) NetViewCounters(tm etime.Times) {
+	if ss.ViewUpdate.View == nil {
+		return
+	}
+	if tm == etime.Trial {
+		ss.TrialStats() // get trial stats for current di
+	}
+	ss.StatCounters()
+	ss.ViewUpdate.Text = ss.Stats.Print([]string{"Run", "Epoch", "Trial", "Cat", "TrialName", "Cycle"})
+}
+
+// TrialStats computes the trial-level statistics.
+// Aggregation is done directly from log data.
+func (ss *Sim) TrialStats() {
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// 		Logging
+
+func (ss *Sim) ConfigLogs() {
+	ss.Stats.SetString("RunName", ss.Params.RunName(0)) // used for naming logs, stats, etc
+
+	ss.Logs.AddCounterItems(etime.Run, etime.Epoch, etime.Trial, etime.Cycle)
+	ss.Logs.AddPerTrlMSec("PerTrlMSec", etime.Run, etime.Epoch, etime.Trial)
+	ss.Logs.AddStatStringItem(etime.AllModes, etime.AllTimes, "RunName")
+	ss.Logs.AddStatStringItem(etime.AllModes, etime.Trial, "TrialName")
+
+	ss.Logs.CreateTables()
+	ss.Logs.SetContext(&ss.Stats, ss.Net)
+	// don't plot certain combinations we don't use
+	ss.Logs.NoPlot(etime.Train, etime.Cycle)
+	ss.Logs.NoPlot(etime.Test, etime.Run)
+	// note: Analyze not plotted by default
+	ss.Logs.SetMeta(etime.Train, etime.Run, "LegendCol", "RunName")
+	ss.Logs.SetMeta(etime.Test, etime.Epoch, "Type", "Bar")
+}
+
+// Log is the main logging function, handles special things for different scopes
+func (ss *Sim) Log(mode etime.Modes, time etime.Times) {
+	if mode.String() != "Analyze" {
+		ss.Context.Mode = mode // Also set specifically in a Loop callback.
+	}
+	dt := ss.Logs.Table(mode, time)
+	row := dt.Rows
+
+	switch {
+	case time == etime.Cycle:
+		return
+	case time == etime.Trial:
+		ss.TrialStats()
+		ss.StatCounters()
+		ss.Logs.LogRow(mode, time, row)
+		return // don't do reg below
+	}
+
+	ss.Logs.LogRow(mode, time, row) // also logs to file, etc
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// 		Gui
+
+// ConfigGUI configures the Cogent Core GUI interface for this simulation.
+func (ss *Sim) ConfigGUI() {
+	title := "Object Recognition"
+	ss.GUI.MakeBody(ss, "objrec", title, `This simulation explores how a hierarchy of areas in the ventral stream of visual processing (up to inferotemporal (IT) cortex) can produce robust object recognition that is invariant to changes in position, size, etc of retinal input images. See <a href="https://github.com/CompCogNeuro/sims/blob/main/ch6/objrec/README.md">README.md on GitHub</a>.</p>`)
+	ss.GUI.CycleUpdateInterval = 10
+
+	nv := ss.GUI.AddNetView("Network")
+	nv.Options.MaxRecs = 300
+	nv.Options.LayerNameSize = 0.03
+	nv.SetNet(ss.Net)
+	ss.ViewUpdate.Config(nv, etime.Phase, etime.Phase)
+
+	cam := &(nv.SceneXYZ().Camera)
+	cam.Pose.Pos.Set(0.0, 1.733, 2.3)
+	cam.LookAt(math32.Vec3(0, 0, 0), math32.Vec3(0, 1, 0))
+
+	ss.GUI.ViewUpdate = &ss.ViewUpdate
+
+	ss.GUI.AddPlots(title, &ss.Logs)
+
+	itb, _ := ss.GUI.Tabs.NewTab("Image")
+	tg := tensorcore.NewTensorGrid(itb).
+		SetTensor(&ss.Envs.ByMode(etime.Train).(*ImgEnv).Vis.ImgTsr)
+	ss.GUI.SetGrid("Image", tg)
+
+	ss.GUI.FinalizeGUI(false)
+}
+
+func (ss *Sim) MakeToolbar(p *tree.Plan) {
+	ss.GUI.AddToolbarItem(p, egui.ToolbarItem{Label: "Init", Icon: icons.Update,
+		Tooltip: "Initialize everything including network weights, and start over.  Also applies current params.",
+		Active:  egui.ActiveStopped,
+		Func: func() {
+			ss.Init()
+			ss.GUI.UpdateWindow()
+		},
+	})
+
+	ss.GUI.AddLooperCtrl(p, ss.Loops, []etime.Modes{etime.Train, etime.Test})
+
+	ss.GUI.AddToolbarItem(p, egui.ToolbarItem{Label: "Test Init", Icon: icons.Update,
+		Tooltip: "Initialize testing to start over -- if Test Step doesn't work, then do this.",
+		Active:  egui.ActiveStopped,
+		Func: func() {
+			ss.Loops.ResetCountersByMode(etime.Test)
+		},
+	})
+
+	ss.GUI.AddToolbarItem(p, egui.ToolbarItem{Label: "Test All",
+		Icon:    icons.PlayArrow,
+		Tooltip: "Tests a large same of testing items and records ActRFs.",
+		Active:  egui.ActiveStopped,
+		Func: func() {
+			if !ss.GUI.IsRunning {
+				ss.GUI.IsRunning = true
+				ss.GUI.UpdateWindow()
+				go ss.RunTestAll()
+			}
+		},
+	})
+
+	ss.GUI.AddToolbarItem(p, egui.ToolbarItem{Label: "Open Trained Wts", Icon: icons.Open,
+		Tooltip: "Opened weights from the first phase of training, which excludes novel objects",
+		Active:  egui.ActiveStopped,
+		Func: func() {
+			ss.Net.OpenWeightsFS(content, "objrec_train1.wts.gz")
+		},
+	})
+
+	////////////////////////////////////////////////
+	tree.Add(p, func(w *core.Separator) {})
+	ss.GUI.AddToolbarItem(p, egui.ToolbarItem{Label: "Reset RunLog",
+		Icon:    icons.Reset,
+		Tooltip: "Reset the accumulated log of all Runs, which are tagged with the ParamSet used",
+		Active:  egui.ActiveAlways,
+		Func: func() {
+			ss.Logs.ResetLog(etime.Train, etime.Run)
+			ss.GUI.UpdatePlot(etime.Train, etime.Run)
+		},
+	})
+	////////////////////////////////////////////////
+	tree.Add(p, func(w *core.Separator) {})
+	ss.GUI.AddToolbarItem(p, egui.ToolbarItem{Label: "New Seed",
+		Icon:    icons.Add,
+		Tooltip: "Generate a new initial random seed to get different results.  By default, Init re-establishes the same initial seed every time.",
+		Active:  egui.ActiveAlways,
+		Func: func() {
+			ss.RandSeeds.NewSeeds()
+		},
+	})
+	ss.GUI.AddToolbarItem(p, egui.ToolbarItem{Label: "README",
+		Icon:    icons.FileMarkdown,
+		Tooltip: "Opens your browser on the README file that contains instructions for how to run this model.",
+		Active:  egui.ActiveAlways,
+		Func: func() {
+			core.TheApp.OpenURL("https://github.com/CompCogNeuro/sims/blob/main/ch6/objrec/README.md")
+		},
+	})
+}
+
+func (ss *Sim) RunGUI() {
+	ss.Init()
+	ss.ConfigGUI()
+	ss.GUI.Body.RunMainWindow()
+}
